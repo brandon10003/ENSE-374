e548df02ebfb4d6e8a9e4f43abaaea6e9a9cdb53
brandon10003 <brandonclarke@uregina.ca>
2018-10-26 13:10:56 -0600

    This is my working singly linked list!

--- src/ListElement.java
+++ src/ListElement.java
@@ -1,13 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Brandon
- */
 public class ListElement {
-    
+	
+	
+	public ListElement next;
+	//private ListEl
+	private int data;
+	
+	
+	public ListElement()
+	{
+		this.next = null;
+		this.data = 0;
+		
+	}
+	
+	
+	public void setData(int data)
+	{
+		this.data = data;
+	}
+
+	
+	public int getData()
+	{
+		return this.data;
+	}  
 }
--- src/UI.java
+++ src/UI.java
@@ -1,13 +1,91 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.io.*;
+import java.util.*;
 
-/**
- *
- * @author Brandon
- */
 public class UI {
-    
+   
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+            Scanner instream = new Scanner(System.in);
+            char in;
+            boolean run = true;
+            int index;
+            int indata;
+            int outdata;
+            int listsize = 0;
+            ListElement head = new ListElement();
+            ListElement pointer = new ListElement();
+            pointer.next = head;
+            
+            
+            do{
+                System.out.println("Please enter a character to continue:");
+                System.out.println("Enter 'a' to add a node");
+                System.out.println("Enter 'g' to get a node");
+                System.out.println("Enter 'd' to delete a node");
+                System.out.println("Enter 'p' to print a list starting from the head");
+                System.out.println("Enter 'q' to quit");
+                
+                in = instream.next().charAt(0);
+                in = Character.toLowerCase(in);
+                
+                switch(in){
+                    case 'a':
+                        System.out.println("Please enter the value to be added to the list");
+                        indata = instream.nextInt();
+                        pointer.next.setData(indata);
+                        pointer.next.next = new ListElement();
+                        pointer.next = pointer.next.next;
+                        listsize++;
+                            
+                        break;
+                    case 'g':
+                        System.out.println("Please enter the index of the link which you wish to retreive the value from");
+                        index = instream.nextInt();
+                        if (index > listsize){
+                            System.out.println("That is not a valid index");
+                            break;
+                        }
+                        pointer.next = head;
+                        for (int i=0; i<index; i++){
+                            pointer.next = pointer.next.next;
+                        }
+                        outdata = pointer.next.getData();
+                        System.out.println("The value at link " + index + " is " + outdata);
+                        break;
+                    case 'd':
+                        System.out.println("Please enter the index of the link which you wish to delete");
+                        index = instream.nextInt();
+                        if (index > listsize){
+                            System.out.println("That is not a valid index");
+                            break;
+                        }
+                        pointer.next = head;
+                        for (int i=0; i<(index-1); i++){
+                            pointer.next = pointer.next.next;
+                        }
+                        pointer.next.next = pointer.next.next.next;
+                        listsize--;
+                        System.out.println("The former link at index " + index + " has been deleted");
+                        break;
+                    case 'p':
+                        System.out.println("The list printed from the head node as is follows");
+                        pointer.next = head;
+                        for (int i=0; i<listsize; i++){
+                            outdata = pointer.next.getData();
+                            System.out.println(outdata + "->");
+                            pointer.next = pointer.next.next;
+                        }
+                        System.out.println("null");
+                        break;
+                    case 'q':
+                        run = false;
+                        break;
+                    default:
+                        System.out.println("That is not a valid input, please enter a valid input");
+                        break;
+                }
+            }while(run);
+	}
 }
